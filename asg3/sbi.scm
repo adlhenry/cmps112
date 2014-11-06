018790708af46ba9efef1a947c09773d5333d195
adlhenry <adlhenry@ucsc.edu>
2014-11-05 18:29:48 -0800

    Add skeleton statement parser

--- asg3/sbi.scm
+++ asg3/sbi.scm
@@ -28,10 +28,29 @@
 ;; Define symbol table hash
 (define symbol-table (make-hash))
 
+;; Statement label hash function
+(define (label-hash label stmt)
+	(if (null? stmt)
+		(hash-set! symbol-table label stmt)
+		(hash-set! symbol-table label (car stmt))
+	)
+)
+
 ;; Symbol table load function
 (define (load-symbol-table program)
-	(map (lambda (line) (printf "~s --> ~s~n"
-	(car line) (cdr line))) program))
+	(map (lambda (line)
+		(let ((stmt (cdr line)))
+			(if (null? stmt)
+				stmt
+				(if (symbol? (car stmt)) 
+					(label-hash (car stmt) (cdr stmt))
+					stmt
+				)
+			)
+		))
+		program
+	)
+)
 
 ;; Program statement read function
 (define (readlist-from-inputfile filename)
@@ -42,15 +61,62 @@
 				  (close-input-port inputfile)
 						 program))))
 
-;; Debug print function
-(define (write-program-by-line filename program)
-	(printf "==================================================~n")
-	(printf "~a: ~s~n" *run-file* filename)
-	(printf "==================================================~n")
-	(printf "(~n")
-	(map (lambda (line) (printf "~s --> ~s~n"
-	(car line) (cdr line))) program)
-	(printf ")~n"))
+;; Print subroutine
+(define (print-stmt printable)
+	(printf "~s~n" (car printable))
+)
+
+;; Statement execute function
+(define (exe-statement stmt)
+	(if (null? stmt)
+		stmt
+		(let ((stmt-type (car stmt))
+			(stmt-args (cdr stmt)))
+			(cond
+			[(equal? stmt-type 'dim) ('dim)]
+			[(equal? stmt-type 'let) ('let)]
+			[(equal? stmt-type 'goto) (print-stmt stmt-args)]
+			[(equal? stmt-type 'if) ('if)]
+			[(equal? stmt-type 'print) (print-stmt stmt-args)]
+			[(equal? stmt-type 'input) ('input)]
+			)
+		)
+	)
+)
+
+;; Program loop function
+(define (run-program program)
+	(map (lambda (line)
+		(let ((stmt (cdr line)))
+			(if (null? stmt)
+				stmt
+				(if (symbol? (car stmt))
+					;; Need check for label --> null
+					(exe-statement (car (cdr stmt)))
+					(exe-statement (car stmt))
+				)
+			)
+		))
+		program
+	)
+)
+
+;; Symbol table print function
+(define (print-symbol-table program)
+	(map (lambda (line)
+		(let ((stmt (cdr line)))
+			(if (null? stmt)
+				stmt
+				(if (hash-has-key? symbol-table (car stmt))
+					(printf "[~s] --> ~s~n" (car stmt) 
+					(hash-ref symbol-table (car stmt)))
+					(car stmt)
+				)
+			)
+		))
+		program
+	)
+)
 
 ;; Program main function
 (define (main arglist)
@@ -58,7 +124,9 @@
 		(usage-exit)
 		(let* ((sbprogfile (car arglist))
 			   (program (readlist-from-inputfile sbprogfile)))
-			  (write-program-by-line sbprogfile program))))
+			  (load-symbol-table program)
+			  (run-program program)
+			  (print-symbol-table program))))
 
 ;; Call main function
 (main (vector->list (current-command-line-arguments)))
